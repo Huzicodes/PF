d251048fb7cdceddf8fbadae12a304854369dd6e
Muhammad Huzaifa <smarthuzaifa969@gmail.com>
2026-09-04 21:49:57 +0500

    Add Task3 of Lab3 of PF

---
+++ lab3/Task3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    float height;
+    char grade;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    printf("Enter height: ");
+    scanf("%f", &height);
+
+    // Tip: The leading space in " %c" consumes any leftover newline character from the previous input in the input buffer
+    printf("Enter grade: ");
+    scanf(" %c", &grade);
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height); // Displays height with exactly 2 decimal places [80]
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
